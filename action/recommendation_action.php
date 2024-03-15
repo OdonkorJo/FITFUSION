6c709f1a60e60c6ac84fc9a49f32af474e54ad48
Miriam_2003 <miriam.appiah@ashesi.edu.gh>
2024-03-15 17:47:57 +0000

    new backend files and frontend addtions

--- action/recommendation_action.php
+++ action/recommendation_action.php
@@ -11,7 +11,7 @@ if (!isset($_POST["submit"])){
             $user="INSERT INTO userexercises (userid, exerciseID, workoutGoalID, userStatus) VALUES (1, $exerciseID, (SELECT workoutGoalID FROM recommendations WHERE exerciseID = $exerciseID), 'incomplete')";
             $result= $con->query($user);}
         if($result){
-            header("Location../view/workout.php");
+            header("Location:../view/workout.php");
         }
         else{
             header("Location:../view/Recommendation.php?msg=please select exerise");
--- functions/get_id_fxn.php
+++ functions/get_id_fxn.php
@@ -16,8 +16,7 @@ function getallrecommendations($con)
 }
 
 
-function testid()
-{
+function testid(){
     global $con;
     if (!isset($_GET['id'])) {
         $all = getallrecommendations($con);
@@ -25,7 +24,7 @@ function testid()
         echo "<div class='container'>";
         foreach ($all as $row) {
             echo "<div class ='cat'>";
-<<<<<<< Updated upstream
+
                 echo"<label>";
                 echo "<input type='checkbox' name = 'exerise[]' value= '".$row['exerciseID']."'>";
                 echo "<span>" . $row['exercisename']. "</span> <br>"; 
@@ -37,19 +36,8 @@ function testid()
         echo "</form>";
 
     exit();
-=======
-            echo "<label>";
-            echo "<input type='checkbox' name='exercise'class='goal-button' value='" . $row['exerciseID'] . "'>";
-            echo "<span>" . $row['exercisename'] . "</span> <br>";
-            echo "</label>";
-            echo "</div>";
-        }
-        echo "</div>";
-        echo "<a href=\"../action/get_selected_recom_action.php?name='submit'\">";
-        echo "<button type='submit' id='submit' name='submit'>Done</button>";
-        echo "</a>";
-        exit();
->>>>>>> Stashed changes
+       
+
     } else {
 
         $work_var = $_GET['id'];
@@ -68,69 +56,35 @@ function testid()
 
             if ($result) {
                 $all = $result->fetch_all(MYSQLI_ASSOC);
+                echo "<form action = \"../action/recommendation_action.php\" method ='post'>";
                 echo "<div class='container'>";
-<<<<<<< Updated upstream
+
                 foreach($all as $row){
-                    echo "<div class ='cat' id = '".$row['exerciseID']."'>";
+                    echo "<div class ='cat'>";
                         echo"<label>";
-                            echo "<input type='checkbox'>";
+                            echo "<input type='checkbox' name = 'exerise[]' value= '".$row['exerciseID']."'>";
                             echo "<span>" . $row['exercisename']. "</span> <br>"; 
                         echo "</label>";
                     echo "</div>"; 
-            }
-=======
-                foreach ($all as $row) {
-                    echo "<div class ='cat'>";
-                    echo "<label>";
-                    echo "<input type='checkbox' name='exercise' class='goal-button' value='" . $row['exerciseID'] . "'>";
-                    echo "<span>" . $row['exercisename'] . "</span> <br>";
-                    echo "</label>";
-                    echo "</div>";
-                }
->>>>>>> Stashed changes
-                echo "</div>";
 
-            }
+            } echo"</div>";
 
 
 
             echo "<br><br><br>";
             $all = getallrecommendations($con);
             echo "<div class='container'>";
-<<<<<<< Updated upstream
+
             foreach($all as $row){
-                echo "<div class ='cat' id = '".$row['exerciseID']."'>";
+                echo "<div class ='cat'>";
                     echo"<label>";
-                        echo "<input type='checkbox'>";
+                        echo "<input type='checkbox' name = 'exerise[]' value= '".$row['exerciseID']."'>";
                         echo "<span>" . $row['exercisename']. "</span> <br>"; 
                     echo "</label>";
                 echo "</div>"; 
         }
-            echo "</div>";   
-        }}}
+        echo "</div>"; 
+        echo "<button type='submit' name='submit'>Done</button>";
+        echo "</form>";  
+        }}}}
     ?>
-=======
-            foreach ($all as $row) {
-                echo "<div class ='cat'>";
-                echo "<label>";
-                echo "<input type='checkbox'name='exercise' class='goal-button' value='" . $row['exerciseID'] . "'>";
-                echo "<span>" . $row['exercisename'] . "</span> <br>";
-                echo "</label>";
-                echo "</div>";
-            }
-            echo "</div>";}\
->>>>>>> Stashed changes
-
-
-
-        }
-
-        echo "<a href=\"../action/get_selected_recom_action.php?name='submit'\">";
-        echo "<button type='submit' id='submit' name='submit'>Done</button>";
-        echo "</a>";
-
-    }
-}
-?>
-
-<!-- echo "<input type='checkbox' class='goal-button' value='" . $row['exercisename'] . "'> <br>"; -->
