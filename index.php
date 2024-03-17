d7754dec267d3a803921a6b597d74a3204bc7a10
OdonkorJo <ancien.odonkor@ashesi.edu.gh>
2024-03-17 23:28:05 +0000

    Landing Page and index page fixed.

--- index.php
+++ index.php
@@ -1,38 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <!-- using boxicon (for external icons) -->
-    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="../css/Homepage.css">
-    
-    <title>FitFusion - Welcome</title>
-</head>
-<body>
-    <!-- Landing Page Content -->
-    <div class="landing-page">
-        <!-- Background Image -->
-        <div class="background-image"></div>
-        
-        <!-- Welcome Text -->
-        <div class="welcome-text">
-            <h1>Welcome to <span id="fit">FitFusion</span></h1>
-            <div class="additional-text-container">
-            <h2 class = "About"> About </h2>
-        <h2 class="additional-text">FitFusion is a user-friendly workout system that recommends exercises to users based on their fitness goal. Want to elevate your fitness and realize the full potential of your body? Sign Up to get started!! Or sign in if you already have an account</h2>
-    </div>
-            <!-- Text and Buttons Container -->
-            <div class="text-buttons-container">
-                <!-- <h2 class="sub-heading">Transform your body, Transform your life!</h2> -->
-                <!-- Login and Sign-up Options -->
-                <div class="auth-buttons">
-                    <a href="../login/login.php" class="btn">Login</a>
-                    <a href="../login/register.php" class="btn">Sign Up</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</body>
-</html>
+<?php 
+header("Location: view/LandingPage.php");
+exit();
+?>
